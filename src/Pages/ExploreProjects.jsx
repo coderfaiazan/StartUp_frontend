a11b080cf2f835b9cb18e245f4ba67f4fd493961
HammadWorks <159044861+HammadWorks@users.noreply.github.com>
2024-11-08 23:38:04 +0530

    third commit

--- src/Pages/ExploreProjects.jsx
+++ src/Pages/ExploreProjects.jsx
@@ -22,7 +22,7 @@ function ExploreProjects() {
       try {
         // Fetch the collections
         const collections = await getCollection(category);
-        setProjects(collections.data);
+        if (collections.data) setProjects(collections?.data);
         // console.log(collections);
       } catch (error) {
         console.error("Error fetching projects:", error);
@@ -67,7 +67,7 @@ function ExploreProjects() {
         </select>
       </div>
 
-      {projects.length > 0 ? (
+      {projects ? (
         <div
           className={
             view === "grid"
