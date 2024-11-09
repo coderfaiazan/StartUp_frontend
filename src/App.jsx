c133ed0808a1bfe3b6273161f91247dc2b57bc05
coderfaiazan <faizan678siddiqui@gmail.com>
2024-11-09 17:31:27 +0530

    BrowserRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { Navbar, Footer, AuthProvider, ChatBot } from "./Components";
 import {
   HomePage,
@@ -17,102 +17,91 @@ import {
 } from "./Pages";
 
 function App() {
-
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <AuthProvider>
-                  <HomePage />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/explore"
-              element={
-                <AuthProvider>
-                  <ExploreProjects />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/project/:id"
-              element={
-                <AuthProvider>
-                  <ProjectDetailsPage />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/start-project"
-              element={
-                <AuthProvider>
-                  <StartProjectPage />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <AuthProvider>
-                  <UserDashboard />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/transactions"
-              element={
-                <AuthProvider>
-                  <TransactionPage />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/profile"
-              element={
-                <AuthProvider>
-                  <ProfilePage />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/admin"
-              element={
-                <AuthProvider>
-                  <AdminDashboard />
-                </AuthProvider>
-              }
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/chat"
-              element={
-                <AuthProvider>
-                  <ChatPage />
-                </AuthProvider>
-              }
-            />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </main>
-        <ChatBot />
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <AuthProvider>
+                <HomePage />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/explore"
+            element={
+              <AuthProvider>
+                <ExploreProjects />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/project/:id"
+            element={
+              <AuthProvider>
+                <ProjectDetailsPage />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/start-project"
+            element={
+              <AuthProvider>
+                <StartProjectPage />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <AuthProvider>
+                <UserDashboard />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/transactions"
+            element={
+              <AuthProvider>
+                <TransactionPage />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <AuthProvider>
+                <ProfilePage />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <AuthProvider>
+                <AdminDashboard />
+              </AuthProvider>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <AuthProvider>
+                <ChatPage />
+              </AuthProvider>
+            }
+          />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+      <ChatBot />
+      <Footer />
+    </div>
   );
 }
 
